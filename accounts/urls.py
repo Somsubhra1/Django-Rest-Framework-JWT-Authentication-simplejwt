63a463cf3fa48c9f07cde86f902d17e217e45ae1
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-25 22:03:04 +0530

    added default token views for jwt auth

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+'''
+default views for jwt
+'''
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+# '''
--- drf_jwt_authentication/settings.py
+++ drf_jwt_authentication/settings.py
@@ -55,6 +55,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 ROOT_URLCONF = 'drf_jwt_authentication.urls'
 
 TEMPLATES = [
