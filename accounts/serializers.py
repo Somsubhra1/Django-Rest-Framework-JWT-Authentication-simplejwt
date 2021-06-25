fe7ff9dcdb0508739284ca99df7fb0026027beb2
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-25 23:02:12 +0530

    bug fixed

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,7 +14,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data['username'], validated_data['email'], validated_data['password'])
+            validated_data['email'], validated_data['password'])
 
         return user
 
